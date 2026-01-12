4e677266c3eb15fed588dcf099a18ee43ee27888
Aurore Collet <auroreclt@gmail.com>
2026-01-12 14:58:20 +0100

    mise en place du projet Svelte, debut inétgration HTML CSS

---
+++ A-LIRE-AVANT-CORRECTION.md
@@ -0,0 +1,28 @@
+# CARNET DE BORD DU PROJET
+## Jour 1 : Wireframe et intégration HTML/CSS dans Svelte
+
+### Todo List :
+- [X] Mettre en place le carnet de bord
+- [X] Faire une pige de style
+- [X] Prepa du fichier pour portfolio
+- [X] Relire le cachier des charges
+- [X] Créer le wireframe (max 1h30)
+- [X] Mettre en place le projet Svelte (30min)
+- [ ] Intégrér l'UI en mobile first (4h)
+- [ ] Push version du fichier pour Portfolio
+
+
+
+### Bilan de fin de journée :
+#### Ce que je n'ai pas réussi à faire
+
+
+#### Les choix que j'ai faits
+J'ai passé un peu plus de temps que prévu sur la maquette, bien qu'en ayant utilisé un template.
+Prévu 1h30 max, j'y ai passé 2h30
+Je pensais être plus rapide sur cet aspect mais cela m'a permis de comprendre un peu mieux Figma
+
+#### Ce que tu referais différemment
+
+
+#### Les prochaines étapes si tu avais eu plus de temps
---
+++ ochatAi/src/App.svelte
@@ -0,0 +1,54 @@
+<!--********************************** SCRIPT PART **********************************-->
+<script>
+</script>
+
+<!--********************************** HTML PART **********************************-->
+<!--*************** HEADER PART ***************-->
+
+<header>
+  <nav>
+    <ul>
+      <li>Reduire</li>
+      <li>Mon profil / Reglages</li>
+    </ul>
+  </nav>
+</header>
+
+<!--*************** MAIN PART ***************-->
+
+<main>
+  <h1>Commence un chat avec <em>O'Chat AI</em></h1>
+
+  <form action="">
+    <label for="ask-question-hp"></label>
+    <input
+      type="text"
+      name=""
+      id="ask-question-hp"
+      placeholder="Poser une question..."
+    />
+    <button type="submit">Envoyer</button>
+  </form>
+</main>
+
+<!--*************** ASIDE PART ***************-->
+
+<aside>
+  <section><button type="button">Nouveau Chat</button></section>
+  <section><h2>Historique</h2></section>
+  <section>
+    <article>
+      <button type="button">Options</button>
+      <h3>Lorem ipsum dolor, sit amet consectetur</h3>
+      <p>
+        Explicabo nam officia in, illum ad quae. Id enim ut illum adipisci non
+        quibusdam sint animi obcaecati sed quas. Eveniet, in porro!
+      </p>
+      <time datetime="2026-01-12">12 janvier 2026</time>
+    </article>
+  </section>
+</aside>
+
+<!--********************************** STYLE PART **********************************-->
+<style>
+</style>
