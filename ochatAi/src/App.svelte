a414d40838f08d3366039fdaf3af7060206add51
Aurore Collet <auroreclt@gmail.com>
2026-01-12 19:19:40 +0100

    integration page accueil globalement ok

--- ochatAi/src/App.svelte
+++ ochatAi/src/App.svelte
@@ -39,17 +39,106 @@
 <!--*************** ASIDE PART ***************-->
 
 <aside>
-  <section><button type="button">Nouveau Chat</button></section>
-  <section><h2>Historique</h2></section>
-  <section>
-    <article>
-      <button type="button">Options</button>
-      <h3>Lorem ipsum dolor, sit amet consectetur</h3>
-      <p>
+  <section id="conv-nav" class="responsive-hidden">
+    <button type="button"
+      ><Icon icon="gravity-ui:plus" width="1rem" height="1rem" />Nouveau Chat</button
+    >
+  </section>
+  <section id="history-nav">
+    <h2>Historique</h2>
+    <label class="history-search">
+      <input type="text" name="" id="history-search" placeholder="Rechercher" />
+      <button type="button" class="search-btn"
+        ><Icon
+          class="search-icon"
+          icon="gravity-ui:magnifier"
+          width="1.5rem"
+          height="1.5rem"
+        /></button
+      ></label
+    >
+  </section>
+  <section id="history-content">
+    <article class="conv-card">
+      <button type="button" class="conv-menu-btn">
+        <h3>Lorem ipsum dolor...</h3>
+        <Icon
+          class="search-icon"
+          icon="gravity-ui:ellipsis"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      </button>
+      <p class="responsive-hidden">
         Explicabo nam officia in, illum ad quae. Id enim ut illum adipisci non
         quibusdam sint animi obcaecati sed quas. Eveniet, in porro!
       </p>
-      <time datetime="2026-01-12">12 janvier 2026</time>
+      <time class="responsive-hidden" datetime="2026-01-12"
+        >12 janvier 2026</time
+      >
+    </article>
+
+    <article class="conv-card">
+      <button type="button" class="conv-menu-btn">
+        <h3>Lorem ipsum dolor...</h3>
+        <Icon
+          class="search-icon"
+          icon="gravity-ui:ellipsis"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      </button>
+      <p class="responsive-hidden">
+        Explicabo nam officia in, illum ad quae. Id enim ut illum adipisci non
+        quibusdam sint animi obcaecati sed quas. Eveniet, in porro! Explicabo
+        nam officia in, illum ad quae. Id enim ut illum adipisci non quibusdam
+        sint animi obcaecati sed quas. Eveniet, in porro!
+      </p>
+      <time class="responsive-hidden" datetime="2026-01-12"
+        >12 janvier 2026</time
+      >
+    </article>
+
+    <article class="conv-card">
+      <button type="button" class="conv-menu-btn">
+        <h3>Lorem ipsum dolor...</h3>
+        <Icon
+          class="search-icon"
+          icon="gravity-ui:ellipsis"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      </button>
+      <p class="responsive-hidden">
+        Explicabo nam officia in, illum ad quae. Id enim ut illum adipisci non
+        quibusdam sint animi obcaecati sed quas. Eveniet, in porro! Explicabo
+        nam officia in, illum ad quae. Id enim ut illum adipisci non quibusdam
+        sint animi obcaecati sed quas. Eveniet, in porro!
+      </p>
+      <time class="responsive-hidden" datetime="2026-01-12"
+        >12 janvier 2026</time
+      >
+    </article>
+
+    <article class="conv-card">
+      <button type="button" class="conv-menu-btn">
+        <h3>Lorem ipsum dolor...</h3>
+        <Icon
+          class="search-icon"
+          icon="gravity-ui:ellipsis"
+          width="1.5rem"
+          height="1.5rem"
+        />
+      </button>
+      <p class="responsive-hidden">
+        Explicabo nam officia in, illum ad quae. Id enim ut illum adipisci non
+        quibusdam sint animi obcaecati sed quas. Eveniet, in porro! Explicabo
+        nam officia in, illum ad quae. Id enim ut illum adipisci non quibusdam
+        sint animi obcaecati sed quas. Eveniet, in porro!
+      </p>
+      <time class="responsive-hidden" datetime="2026-01-12"
+        >12 janvier 2026</time
+      >
     </article>
   </section>
 </aside>
@@ -75,7 +164,7 @@
   }
 
   @media screen and (min-width: 768px) {
-    .responsive-hidden {
+    header .responsive-hidden {
       display: initial;
     }
 
@@ -125,7 +214,154 @@
 
   @media screen and (min-width: 1024px) {
     main {
-      max-width: 40vw;
+      position: absolute;
+      left: 20vw;
+      width: 100%;
+      padding: 0 15rem;
+      margin: auto;
+    }
+  }
+
+  /* *************** ASIDE PART *************** */
+  aside {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  #history-nav {
+    display: flex;
+    gap: 0.2rem;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .history-search {
+    display: flex;
+  }
+  #history-search {
+    max-width: 45vw;
+  }
+
+  #history-content {
+    display: flex;
+    flex-direction: column;
+    padding: 1rem 0;
+    gap: 0.5rem;
+  }
+
+  .conv-card {
+    display: flex;
+    flex-direction: column;
+    border: solid 1px rgb(180, 180, 180);
+    border-radius: 2rem;
+    padding: 0 0.5rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .conv-menu-btn {
+    display: flex;
+    padding: 1rem;
+    justify-content: space-between;
+    border-radius: 2rem;
+  }
+
+  h3 {
+    color: var(--primarycolor);
+  }
+
+  @media screen and (min-width: 768px) {
+    aside section {
+      max-width: 80vw;
+      margin: auto;
+    }
+
+    #history-nav {
+      width: 80vw;
+    }
+
+    #history-search {
+      width: 40vw;
+    }
+
+    #history-content {
+      width: 80vw;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+
+      .responsive-hidden {
+        display: initial;
+      }
+    }
+    .conv-card {
+      max-height: 13rem;
+      justify-content: space-between;
+    }
+
+    p {
+      padding: 0 1rem;
+      overflow: scroll;
+    }
+
+    time {
+      color: rgb(130, 130, 130);
+      padding: 0.5rem 1rem;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    .responsive-hidden {
+      display: block;
+    }
+
+    aside {
+      position: absolute;
+      top: 2.5rem;
+      width: 20vw;
+      overflow: scroll;
+    }
+
+    #conv-nav button {
+      display: flex;
+      align-items: center;
+      padding: 0.5rem 1.2rem;
+      gap: 0.5rem;
+      background-color: var(--primarycolor);
+      color: var(--lightcolor);
+    }
+
+    #history-nav {
+      margin: 1rem 0;
+      padding: 1rem 0;
+      display: initial;
+      height: 8rem;
+      width: 100%;
+    }
+
+    #history-search {
+      width: 90%;
+    }
+
+    #history-content {
+      width: 100%;
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+    }
+
+    #history-content .responsive-hidden {
+      display: none;
+    }
+
+    .conv-card {
+      h3 {
+        max-width: 60%;
+        max-height: 2rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
     }
   }
 </style>
